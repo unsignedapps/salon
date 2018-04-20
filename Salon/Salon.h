b90fe27f5ac4e948217cc781d0afa18dd5494eee
Rob Amos <bok@unsignedapps.com>
2018-04-20 16:47:28 +1000

    Initial Commit

---
+++ Salon/Salon.h
@@ -0,0 +1,19 @@
+//
+//  Salon.h
+//  Salon
+//
+//  Created by Rob Amos on 20/4/18.
+//  Copyright © 2018 Unsigned Apps Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Salon.
+FOUNDATION_EXPORT double SalonVersionNumber;
+
+//! Project version string for Salon.
+FOUNDATION_EXPORT const unsigned char SalonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Salon/PublicHeader.h>
+
+
